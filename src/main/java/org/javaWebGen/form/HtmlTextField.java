3e7e30441be70d24335dd55d61f911f67ff9e162
kevscott_tx@yahoo.com <kevscott_tx@yahoo.com>
2018-04-14 22:04:51 -0500

    Misc Doco updates

--- src/main/java/org/javaWebGen/form/HtmlTextField.java
+++ src/main/java/org/javaWebGen/form/HtmlTextField.java
@@ -1,3 +1,42 @@
+/*
+ * =================================================================== *
+ * Copyright (c) 2017 Kevin Scott All rights  reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ * if any, must include the following acknowledgment:
+ * "This product includes software developed by "Kevin Scott"
+ * Alternately, this acknowledgment may appear in the software itself,
+ * if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The name "Kevin Scott must not be used to endorse or promote products
+ * derived from this software without prior written permission. For
+ * written permission, please contact kevscott_tx@yahoo.com
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL KEVIN SCOTT BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ */
 package org.javaWebGen.form;
 
 import org.apache.commons.text.StringEscapeUtils;
@@ -46,11 +85,12 @@ public class HtmlTextField extends HtmlField{
  	 
 		return htmlBuffer.toString();
 	}
-	@Override
-	public boolean validate(String value){ //maybe check for odd encoding?
-		return true;
+	/*@Override
+	public boolean validate(String value){ 
+		boolean val=super.validate(value);
+		return val;
 		
-	}
+	}*/
 	@Override
 	public void cleanField() {
 		if(this.getValue()!=null){
@@ -81,7 +121,7 @@ public class HtmlTextField extends HtmlField{
 		StringBuffer json=new StringBuffer("");
 		if(this.isRequired() ){
 			json.append(this.getName()+":{\n");
-			json.append("    minlength: 2,\n");
+			json.append("    minlength: 1,\n");
 			json.append("    required: true,\n");
 			json.append("},\n");
 		}
